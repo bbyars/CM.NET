fde308dd4cfd9ebcd657230662768bf27fcbebad
Brandon Byars <brandon.byars@gmail.com>
2009-09-27 15:48:19 -0600

    Adding some global exception handling to hopefully get a better idea of why it's failing

--- CM.Deploy.UI/App.xaml.cs
+++ CM.Deploy.UI/App.xaml.cs
@@ -1,6 +1,18 @@
-﻿namespace CM.Deploy.UI
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace CM.Deploy.UI
 {
     public partial class App
     {
+        public App()
+        {
+            DispatcherUnhandledException += AppDispatcherUnhandledException;
+        }
+
+        private static void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
+        }
     }
 }
