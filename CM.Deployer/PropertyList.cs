b0c9957dd07131711fa659a0a3dab2a7360f3b0b
U-WIN-BU4OBTL8E4V\bbyars <bbyars@WIN-BU4OBTL8E4V.(none)>
2010-03-05 22:34:18 -0700

    Finished moving everything to PropertyList, and trimmed down interface

--- CM.Deployer/PropertyList.cs
+++ CM.Deployer/PropertyList.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +9,7 @@ namespace CM.Deployer
     /// property values may depend on properties already defined.  Therefore, a dictionary
     /// isn't really an appropriate absraction...
     /// </summary>
-    public class PropertyList : IList<KeyValuePair<string, string>>
+    public class PropertyList : IEnumerable<KeyValuePair<string, string>>
     {
         private readonly List<KeyValuePair<string, string>> properties = new List<KeyValuePair<string, string>>();
 
@@ -18,6 +17,12 @@ namespace CM.Deployer
         {
             properties.Add(new KeyValuePair<string, string>(key, value));
             return this;
+        }
+
+        public override string ToString()
+        {
+            var lines = properties.Select(prop => string.Format("[{0}: {1}]", prop.Key, prop.Value)).ToArray();
+            return string.Join(", ", lines);
         }
 
         public override bool Equals(object obj)
@@ -33,12 +38,6 @@ namespace CM.Deployer
             return (properties != null ? properties.GetHashCode() : 0);
         }
 
-        public override string ToString()
-        {
-            var lines = properties.Select(prop => string.Format("[{0}: {1}]", prop.Key, prop.Value)).ToArray();
-            return string.Join(", ", lines);
-        }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
@@ -47,62 +46,6 @@ namespace CM.Deployer
         public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
         {
             return properties.GetEnumerator();
-        }
-
-        public void Add(KeyValuePair<string, string> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(KeyValuePair<string, string> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(KeyValuePair<string, string> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Count
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public bool IsReadOnly
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public int IndexOf(KeyValuePair<string, string> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Insert(int index, KeyValuePair<string, string> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveAt(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public KeyValuePair<string, string> this[int index]
-        {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
         }
     }
 }
